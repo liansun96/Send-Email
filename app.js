92b75791cf0d4d62b119a64a091975cedd3fd76b
liansun96 <liansun0203@gmail.com>
2025-02-19 18:11:05 +0630

    setup middleware and controller

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json())
+
+
 const port = process.env.PORT || 5000
 
 const start = async () => {
