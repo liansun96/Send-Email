669a4e9767efa386e1e35c97a8bc608224463946
liansun96 <liansun0203@gmail.com>
2025-02-22 23:07:11 +0630

    testing nodemailer

---
+++ controller/sendEmail.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer');
+
+const sendEmailEthereal = async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'marlene.legros@ethereal.email',
+      pass: 'va4q5BKKtry7aq58Gv', 
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Coding Addict" <codingaddict@gmail.com>',
+    to: 'bar@example.com',
+    subject: 'Hello',
+    html: '<h2>Sending Emails with Node.js</h2>',
+  });
+
+  res.json(info);
+};
